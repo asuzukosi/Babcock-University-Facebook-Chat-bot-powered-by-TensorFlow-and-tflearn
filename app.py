8e4a8e6b408f7f90dfda146cb7af933718ce68f3
asuzukosi <keloasuzu@yahoo.com>
2020-02-29 11:43:54 +0000

    Chatbot

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
-import tensorflow as tf
+import os
+import sys
 import requests
 from flask import Flask, request
-import pickle
+import tensorflow as tf
+import tflearn
 import numpy as np
+import random
 import json
 import nltk
-nltk.download('punkt')
-import random
 from nltk.stem.lancaster import LancasterStemmer
-import tflearn
-import os
+import pickle
 
 
 stemmer = LancasterStemmer()
 
+classes = ["greeting", "goodbye", "age", "name", "location", "state",  "function", "food",  "price-food", "want",  "babcock", "location-babcock", "history","departments-babcock", "core-values", "aim", "sat-location", "bbs-location", "sabbath", "dorcasing"]
+
 with open("intents2.json") as f:
     data = json.load(f)
 
+# print(data["intents"])
 try:
-
     with open("data.pickle", "rb") as f:
         words, labels, training, output = pickle.load(f)
 
 except:
-
     words = []
     labels = []
     docs_x = []
     docs_y = []
-
     for intent in data["intents"]:
         for pattern in intent["patterns"]:
             wrds = nltk.word_tokenize(pattern)
@@ -71,6 +71,8 @@ except:
     with open("data.pickle", "wb") as f:
         pickle.dump((words, labels, training, output), f)
 
+    # print(output)
+
 tf.reset_default_graph()
 
 net = tflearn.input_data(shape=[None, len(training[0])])
@@ -81,13 +83,11 @@ net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
 net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
-
 '''
 model.fit(training, output, n_epoch=3000, batch_size=8, show_metric="True")
-model.save("BusaHelp.h5")
+model.save("busabot.h5")
 '''
-model.load("BusaHelp.h5")
-
+model.load("busabot.h5")
 
 
 def bag_of_words(s, words):
@@ -104,93 +104,30 @@ def bag_of_words(s, words):
     return np.array(bag)
 
 
-# create a data structure to hold user context
-context = {}
+def chat(message, id):
 
-ERROR_THRESHOLD = 0.65
+    prediction = model.predict([bag_of_words(message, words)])
 
+    result = prediction[0][np.argmax(prediction)]
 
-def classify(sentence):
-    # generate probabilities from the model
-    p = bag_of_words(sentence, words)
+    if result > 0.7:
 
-    d = len(p)
-    f = len(docs_x) - 2
-    a = np.zeros([f, d])
-    tot = np.vstack((p, a))
+        tag = labels[np.argmax(prediction)]
 
-    results = model.predict(tot)[0]
+        response = ""
 
-    # filter out predictions below a threshold
-    results = [[i, r] for i, r in enumerate(results) if r > ERROR_THRESHOLD]
-    # sort by strength of probability
-    results.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append((labels[r[0]], r[1]))
-    # return tuple of intent and probability
-    return return_list
+        for tg in data["intents"]:
+            if tg["tag"] == tag:
+                responses = tg["responses"]
 
+                response = random.choice(responses)
 
-def response(sentence, userID, show_details=False):
-    results = classify(sentence)
-    print('Result:', results)
-    # if we have a classification then find the matching intent tag
-    if results:
-        # loop as long as there are matches to process
-        while results:
-            for i in data['intents']:
-                # find a tag matching the first result
-                if i['tag'] == results[0][0]:
-                    # set context for this intent if necessary
-                    if 'context_set' in i:
-                        if show_details: print('context:', i['context_set'])
-                        context[userID] = i['context_set']
+        return(response)
 
-                    # check if this intent is contextual and applies to this user's conversation
-                    if not 'context_filter' in i or \
-                            (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
-                        if show_details: print('tag:', i['tag'])
-                        # a random response from the intent
-                        return (random.choice(i['responses']))
-            results.pop(0)
+    else:
+        response = "I didnt quite get that please ask another question"
+        return (response)
 
-
-'''
-def chat():
-    print("Kosi bot is ready to go")
-
-    while True:
-        inp = input("You: ")
-        inp = inp.lower()
-
-        if inp == "quit":
-            break
-
-        prediction = model.predict([bag_of_words(inp, words)])
-
-        result = prediction[0][np.argmax(prediction)]
-
-        if result > 0.7:
-
-            tag = labels[np.argmax(prediction)]
-
-            response = ""
-
-            for tg in data["intents"]:
-                if tg["tag"] == tag:
-                    responses = tg["responses"]
-
-                    response = random.choice(responses)
-
-            print("Kosi says:", response)
-
-        else:
-            print("Kosi says: I don't quite understand what you are saying, ask another question please...")
-
-
-chat()
-'''
 
 app = Flask(__name__)
 
@@ -204,12 +141,11 @@ def verify():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-    return "Busa Chat Bot ", 200
+    return "Hello from Babcock Bot", 200
 
 
 @app.route('/', methods=['POST'])
 def webhook():
-
     # endpoint for processing incoming messaging events
     data = request.get_json()
     print(data)  # you may not want to log every incoming message in production, but it's good for testing
@@ -224,9 +160,12 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    responseai = response(message_text, sender_id)
-                    send_message(sender_id, responseai)
+                    print(sender_id)
+                    print(recipient_id)
+                    print(message_text)
+                    
+                    response = chat(message_text, sender_id)
+                    send_message(sender_id, response)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -237,12 +176,12 @@ def webhook():
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                     pass
 
-    return "ok", 200
+    return "page data received ", 200
 
 
 def send_message(recipient_id, message_text):
 
-    print("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    # print("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
@@ -250,7 +189,7 @@ def send_message(recipient_id, message_text):
     headers = {
         "Content-Type": "application/json"
     }
-    data = json.dumps({
+    info = json.dumps({
         "recipient": {
             "id": recipient_id
         },
@@ -258,10 +197,11 @@ def send_message(recipient_id, message_text):
             "text": message_text
         }
     })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=info)
     if r.status_code != 200:
         print(r.status_code)
         print(r.text)
+    return("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
 
 if __name__ == '__main__':
